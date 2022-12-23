c0addfe9676a03a0ce991865378c2450e98ff227
“billyseel” <faizalre@yahoo.com.br>
2022-12-23 16:52:22 +0800

    Add code

--- todo/settings.py
+++ todo/settings.py
@@ -120,6 +120,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
